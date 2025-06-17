7a6799a06771b6790c258f1596804530e96b87d1
Juan Perpiña <jperpi01@gmail.com>
2025-06-17 15:15:15 +0200

    Logica BBDD

---
+++ lib/db/db_provider.dart
@@ -0,0 +1,30 @@
+
+
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider {
+  static Database? _database;
+
+  static Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  static Future<Database> _initDB() async {
+    Directory documentsDirectory = await getApplicationCacheDirectory();
+    String path = join(documentsDirectory.path, 'fontper.db');
+
+    if (!File(path).existsSync()) {
+      ByteData data = await rootBundle.load('assets/fontper.db');
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(path).writeAsBytes(bytes, flush: true);
+    }
+return await openDatabase(path, readOnly: false);
+  }
+}
